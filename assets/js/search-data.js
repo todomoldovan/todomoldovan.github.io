f3eb4868575ceba06a29c45dc29b90e329e91933
todomoldovan <79604880+todomoldovan@users.noreply.github.com>
2026-03-30 18:30:24 +0000

    Deploying to gh-pages from @ todomoldovan/todomoldovan.github.io@974108cad36c300d72591490aa160930bb009eef 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,13 +18,13 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
+          description: "This page displays the list of courses I am currently teaching. Check my [resume](/cv/) for a full list of courses I have assisted on.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
           },
         },{id: "nav-resume",
-          title: "resume",
+          title: "Resume",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
